c73c7ec64f69c6a2fb86c1eea90f384551b80be8
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2023-07-26 23:08:50 +0300

    object definition

---
+++ 02Basics/myobjects.js
@@ -0,0 +1,10 @@
+"use strict";
+// const user = {
+//     name: "kevin",
+//     email: "kevin@gmail.com",
+//     isActive: true
+// }
+Object.defineProperty(exports, "__esModule", { value: true });
+function createUser(user) {
+    return { name: "", email: "", isActive: true };
+}
