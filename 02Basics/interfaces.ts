87cc845ad40595c2b4d540dd706ea073f4365262
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2023-08-03 13:08:21 +0300

    interfaces

--- 02Basics/interfaces.ts
+++ 02Basics/interfaces.ts
@@ -7,10 +7,21 @@ interface User {
     getCoupon(couponName: string, value: number): number
 }
 
-const kevin: User = {
+interface User {
+    githubToken: string
+}
+
+interface Admin extends User {
+    role: "admin" | "ta" | "learner"
+}
+
+const kevin: Admin = {
     dbId: 22,
     email: "email@gmail.com",
+    role: "admin",
     userId: 2211,
+    githubToken: 'github',
     startTrial: () => {return "trial started"},
-    getCoupon: (name: "kk", value: 5) => { return 10 }
+    getCoupon: (couponName: "kk", value: 5) => { return 10 }
 }
+
